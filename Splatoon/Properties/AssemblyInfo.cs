b414ca5e5047873995f28dc2c6717c22118bc543
Eternita <lysithea.star@gmail.com>
2021-08-03 01:23:41 +0300

    ver

--- Splatoon/Properties/AssemblyInfo.cs
+++ Splatoon/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.1.6")]
+[assembly: AssemblyVersion("0.1.1.7")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
