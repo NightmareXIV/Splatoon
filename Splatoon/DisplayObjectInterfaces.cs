c009c9bdb1a86f6298f810099aed1b33daddc72d
Eternita <lysithea.star@gmail.com>
2021-08-04 13:24:19 +0300

    refactor 2 before it's too late

---
+++ Splatoon/DisplayObjectInterfaces.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splatoon
+{
+    internal class DisplayObjectDot : DisplayObject
+    {
+        public float x, y, z, thickness;
+        public uint color;
+
+        public DisplayObjectDot(float x, float y, float z, float thickness, uint color)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.thickness = thickness;
+            this.color = color;
+        }
+    }
+
+    internal class DisplayObjectCircle : DisplayObject
+    {
+        public float x, y, z, radius, thickness;
+        public uint color;
+        public DisplayObjectCircle(float x, float y, float z, float radius, float thickness, uint color)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.radius = radius;
+            this.thickness = thickness;
+            this.color = color;
+        }
+    }
+
+    internal class DisplayObjectLine : DisplayObject
+    {
+        public float ax, ay, az, bx, by, bz, thickness;
+        public uint color;
+
+        public DisplayObjectLine(float ax, float ay, float az, float bx, float by, float bz, float thickness, uint color)
+        {
+            this.ax = ax;
+            this.ay = ay;
+            this.az = az;
+            this.bx = bx;
+            this.by = by;
+            this.bz = bz;
+            this.thickness = thickness;
+            this.color = color;
+        }
+    }
+
+    internal class DisplayObjectText : DisplayObject
+    {
+        public float x, y, z;
+        public string text;
+        public uint bgcolor, fgcolor;
+
+        public DisplayObjectText(float x, float y, float z, string text, uint bgcolor, uint fgcolor)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.text = text;
+            this.bgcolor = bgcolor;
+            this.fgcolor = fgcolor;
+        }
+    }
+
+    internal interface DisplayObject { }
+
+}
--- Splatoon/Gui.cs
+++ Splatoon/Gui.cs
@@ -49,29 +49,21 @@ namespace Splatoon
                     ImGui.SetWindowSize(ImGui.GetIO().DisplaySize);
                     foreach (var element in p.displayObjects)
                     {
-                        if (element is DisplayObjectCircle)
+                        if (element is DisplayObjectCircle elementCircle)
                         {
-                            var e = (DisplayObjectCircle)element;
-                            DrawRingWorld(e.x, e.y, e.z, e.radius, p.Config.segments, e.thickness, e.color);
+                            DrawRingWorld(elementCircle);
                         }
-                        else if (element is DisplayObjectDot)
+                        else if (element is DisplayObjectDot elementDot)
                         {
-                            var e = (DisplayObjectDot)element;
-                            DrawPoint(e.x, e.y, e.z, e.thickness, e.color);
+                            DrawPoint(elementDot);
                         }
-                        else if (element is DisplayObjectText)
+                        else if (element is DisplayObjectText elementText)
                         {
-                            var e = (DisplayObjectText)element;
-                            DrawTextWorld(e.x, e.y, e.z, e.text, e.bgcolor, e.fgcolor);
+                            DrawTextWorld(elementText);
                         }
-                        else if (element is DisplayObjectLine)
+                        else if (element is DisplayObjectLine elementLine)
                         {
-                            var e = (DisplayObjectLine)element;
-                            p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(e.ax, e.az, e.ay), out SharpDX.Vector2 pos);
-                            ImGui.GetWindowDrawList().PathLineTo(new Num.Vector2(pos.X, pos.Y));
-                            p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(e.bx, e.bz, e.by), out SharpDX.Vector2 pos2);
-                            ImGui.GetWindowDrawList().PathLineTo(new Num.Vector2(pos2.X, pos2.Y));
-                            ImGui.GetWindowDrawList().PathStroke(e.color, ImDrawFlags.None, e.thickness);
+                            DrawLineWorld(elementLine);
                         }
                     }
                     ImGui.End();
@@ -91,41 +83,50 @@ namespace Splatoon
             }
         }
 
-        public void DrawTextWorld(float x, float y, float z, string text, uint bgcolor, uint fgcolor)
+        void DrawLineWorld(DisplayObjectLine e)
+        {
+            p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(e.ax, e.az, e.ay), out SharpDX.Vector2 pos);
+            ImGui.GetWindowDrawList().PathLineTo(new Num.Vector2(pos.X, pos.Y));
+            p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(e.bx, e.bz, e.by), out SharpDX.Vector2 pos2);
+            ImGui.GetWindowDrawList().PathLineTo(new Num.Vector2(pos2.X, pos2.Y));
+            ImGui.GetWindowDrawList().PathStroke(e.color, ImDrawFlags.None, e.thickness);
+        }
+
+        public void DrawTextWorld(DisplayObjectText e)
         {
             if (p.pi.Framework.Gui.WorldToScreen(
-                            new SharpDX.Vector3(x, z, y),
+                            new SharpDX.Vector3(e.x, e.z, e.y),
                             out SharpDX.Vector2 pos))
             {
-                var size = ImGui.CalcTextSize(text);
+                var size = ImGui.CalcTextSize(e.text);
                 size = new Num.Vector2(size.X + 10f, size.Y + 10f);
                 ImGui.SetNextWindowPos(new Num.Vector2(pos.X - size.X / 2, pos.Y - size.Y / 2));
                 ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, new Num.Vector2(5, 5));
                 ImGui.PushStyleVar(ImGuiStyleVar.ChildRounding, 10f);
-                ImGui.PushStyleColor(ImGuiCol.ChildBg, ImGui.ColorConvertU32ToFloat4(bgcolor));
-                ImGui.BeginChild("##child" + text + ++uid, size, false,
+                ImGui.PushStyleColor(ImGuiCol.ChildBg, ImGui.ColorConvertU32ToFloat4(e.bgcolor));
+                ImGui.BeginChild("##child" + e.text + ++uid, size, false,
                     ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoNav
                     | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.AlwaysUseWindowPadding);
-                ImGui.TextColored(ImGui.ColorConvertU32ToFloat4(fgcolor), text);
+                ImGui.TextColored(ImGui.ColorConvertU32ToFloat4(e.fgcolor), e.text);
                 ImGui.EndChild();
                 ImGui.PopStyleColor();
                 ImGui.PopStyleVar(2);
             }
         }
 
-        public void DrawRingWorld(float x, float y, float z, float radius, int num_segments, float thicc, uint colour)
+        public void DrawRingWorld(DisplayObjectCircle e)
         {
-            int seg = num_segments / 2;
-            p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(x + (radius * (float)Math.Sin(p.CamAngleX)), z,
-            y + (radius * (float)Math.Cos(p.CamAngleX))), out SharpDX.Vector2 refpos);
+            int seg = p.Config.segments / 2;
+            p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(e.x + (e.radius * (float)Math.Sin(p.CamAngleX)), e.z,
+            e.y + (e.radius * (float)Math.Cos(p.CamAngleX))), out SharpDX.Vector2 refpos);
             var visible = false;
-            Num.Vector2?[] elements = new Num.Vector2?[num_segments+1];
-            for (int i = 0; i <= num_segments; i++)
+            Num.Vector2?[] elements = new Num.Vector2?[seg+1];
+            for (int i = 0; i <= seg; i++)
             {
                 visible = p.pi.Framework.Gui.WorldToScreen(
-                    new SharpDX.Vector3(x + (radius * (float)Math.Sin((Math.PI / seg) * i)),
-                    z,
-                    y + (radius * (float)Math.Cos((Math.PI / seg) * i))),
+                    new SharpDX.Vector3(e.x + (e.radius * (float)Math.Sin((Math.PI / seg) * i)),
+                    e.z,
+                    e.y + (e.radius * (float)Math.Cos((Math.PI / seg) * i))),
                     out SharpDX.Vector2 pos) 
                     || visible;
                 if (pos.Y > refpos.Y) elements[i] = new Num.Vector2(pos.X, pos.Y);
@@ -137,85 +138,18 @@ namespace Splatoon
                     if (pos == null) continue;
                     ImGui.GetWindowDrawList().PathLineTo(pos.Value);
                 }
-                ImGui.GetWindowDrawList().PathStroke(colour, ImDrawFlags.Closed, thicc);
+                ImGui.GetWindowDrawList().PathStroke(e.color, ImDrawFlags.Closed, e.thickness);
             }
         }
 
-        public void DrawPoint(float x, float y, float z, float thicc, uint col)
+        public void DrawPoint(DisplayObjectDot e)
         {
-            if(p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(x, z, y), out SharpDX.Vector2 pos)) 
+            if(p.pi.Framework.Gui.WorldToScreen(new SharpDX.Vector3(e.x, e.z, e.y), out SharpDX.Vector2 pos)) 
                 ImGui.GetWindowDrawList().AddCircleFilled(
                 new Num.Vector2(pos.X, pos.Y),
-                thicc,
-                ImGui.GetColorU32(col),
+                e.thickness,
+                ImGui.GetColorU32(e.color),
                 100);
         }
     }
-
-    internal class DisplayObjectDot : DisplayObject
-    {
-        public float x, y, z, thickness;
-        public uint color;
-
-        public DisplayObjectDot(float x, float y, float z, float thickness, uint color)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.thickness = thickness;
-            this.color = color;
-        }
-    }
-
-    internal class DisplayObjectCircle : DisplayObject
-    {
-        public float x, y, z, radius, thickness;
-        public uint color;
-        public DisplayObjectCircle(float x, float y, float z, float radius, float thickness, uint color)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.radius = radius;
-            this.thickness = thickness;
-            this.color = color;
-        }
-    }
-
-    internal class DisplayObjectLine : DisplayObject
-    {
-        public float ax, ay, az, bx, by, bz, thickness;
-        public uint color;
-
-        public DisplayObjectLine(float ax, float ay, float az, float bx, float by, float bz, float thickness, uint color)
-        {
-            this.ax = ax;
-            this.ay = ay;
-            this.az = az;
-            this.bx = bx;
-            this.by = by;
-            this.bz = bz;
-            this.thickness = thickness;
-            this.color = color;
-        }
-    }
-
-    internal class DisplayObjectText : DisplayObject
-    {
-        public float x, y, z;
-        public string text;
-        public uint bgcolor, fgcolor;
-
-        public DisplayObjectText(float x, float y, float z, string text, uint bgcolor, uint fgcolor)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.text = text;
-            this.bgcolor = bgcolor;
-            this.fgcolor = fgcolor;
-        }
-    }
-
-    internal interface DisplayObject { }
 }
