a2e89e90b46ecc75e5d3ad87e599068a58d59d3a
Limiana <5073202+Limiana@users.noreply.github.com>
2022-12-07 16:46:11 +0300

    director update

--- Splatoon/Memory/DirectorUpdateProcessor.cs
+++ Splatoon/Memory/DirectorUpdateProcessor.cs
@@ -22,7 +22,7 @@ namespace Splatoon.Memory
             {
                 if (P.Config.Logging)
                 {
-                    var text = $"Director Update: {a3:X8}, {a4:X8}, {a5:X8}, {a6:X8}, {a7:X8}";
+                    var text = $"Director Update: {a3:X}, {a4:X8}, {a5:X8}, {a6:X8}, {a7:X8}";
                     Logger.Log(text);
                 }
                 ScriptingProcessor.OnDirectorUpdate(a3);
