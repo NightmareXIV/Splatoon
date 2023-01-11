2eda6f2e1a4f318a80d7feaa66bc95917c943edc
Limiana <5073202+Limiana@users.noreply.github.com>
2023-01-11 14:47:29 +0300

    6.3 p2

--- Splatoon/Memory/TetherProcessor.cs
+++ Splatoon/Memory/TetherProcessor.cs
@@ -8,7 +8,7 @@ namespace Splatoon.Memory;
 internal unsafe class TetherProcessor
 {
     delegate long ProcessTether(Character* a1, byte a2, byte a3, long targetOID, byte a5);
-    [Signature("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 41 54 41 55 41 56 41 57 48 83 EC 20 0F B6 6C 24", DetourName = nameof(ProcessTetherDetour), Fallibility = Fallibility.Fallible)]
+    [Signature("E8 ?? ?? ?? ?? EB 48 41 81 FF", DetourName = nameof(ProcessTetherDetour), Fallibility = Fallibility.Fallible)]
     Hook<ProcessTether> ProcessTetherHook = null;
 
     internal TetherProcessor()
